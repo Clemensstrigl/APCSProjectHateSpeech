04a7625778bef1b1197b04f01ebd5261cf210169
Clemens Strigl <cstrigl928@Student.fuhsd.org>
2019-10-24 14:58:03 -0700

    Added Hash map to map the data

--- scr/src/Main.java
+++ scr/src/Main.java
@@ -1,6 +1,9 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -9,18 +12,19 @@ public class Main {
     static ArrayList<String> Slangs = new ArrayList<String>();
     static ArrayList<String> Sentences = new ArrayList<String>();
     static ArrayList<String> Symbols = new ArrayList<String>();
+    private static Object ArrayList;
 
 
     public static void main(String[] args) {
-        importDataSet("C:\\Users\\cleme\\IdeaProjects\\APCSProjcetHateSpeech\\scr\\All Data\\DiscriminativeDataBase");
+        var dataByName = importDataSet("C:\\Users\\cleme\\IdeaProjects\\APCSProjcetHateSpeech\\scr\\All Data\\DiscriminativeDataBase");
+        System.out.println(dataByName.size());
+        System.out.println(dataByName.get("Races"));
 
 }
 
 
-
-
-
-    public static String importDataSet(String filename) {
+    public static HashMap importDataSet(String filename) {
+        var dataByName = new HashMap<String, List<String>>();
         Scanner scanner;
         StringBuilder output = new StringBuilder();
 
@@ -29,19 +33,20 @@ public class Main {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] splitLines = line.split(",");
-                ArrayList<String> Lines = (ArrayList) Main.class.getField("splitLines[0]");
-                for (int i = 0; i < splitLines.length; i++) {
-
+                var dataList = new ArrayList<String>();
+                for (int s = 1; s < splitLines.length; s++) {
+                    dataList.add(splitLines[s]);
                 }
+                dataByName.put(splitLines[0], dataList);
 
             }
 
             scanner.close();
 
-        } catch (FileNotFoundException | NoSuchFieldException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found " + filename);
         }
 
-        return output.toString();
+        return dataByName;
     }
 }
